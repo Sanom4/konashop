c2c05223e3f0593e7f9b37d9d0c2cecd5e8d0ccc
sanom4 <alexander@gonzalez.co.il>
2023-12-05 13:17:11 +0200

    push debug

--- wp-config.php
+++ wp-config.php
@@ -102,7 +102,7 @@ $table_prefix = 'kona_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* Add any custom values between this line and the "stop editing" line. */
 
